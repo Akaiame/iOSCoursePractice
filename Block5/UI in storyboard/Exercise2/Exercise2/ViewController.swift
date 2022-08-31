8bcb213b845a55e324d2c2dde4f0cee10d7b9ee9
Akaiame <dmitryilichov@gmail.com>
2022-09-01 00:33:31 +0300

    add exercise 2 UI in storyboard

---
+++ Block5/UI in storyboard/Exercise2/Exercise2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise2
+//
+//  Created by Дмитрий Ильичёв on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

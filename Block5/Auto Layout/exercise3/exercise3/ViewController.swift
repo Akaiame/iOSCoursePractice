6ee5ee6cb5445ac97a1dbbe140af59617bb1fb25
Akaiame <dmitryilichov@gmail.com>
2022-09-02 20:51:26 +0300

    add third exercise autolayout

---
+++ Block5/Auto Layout/exercise3/exercise3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise3
+//
+//  Created by Дмитрий Ильичёв on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

cb6929d2e3b67959ffc3678c66f0b965df1ad896
Akaiame <dmitryilichov@gmail.com>
2022-09-07 23:31:03 +0300

    add task 2 animation

--- Block6/Animations/animation/animation/SecondViewController.swift
+++ Block6/Animations/animation/animation/SecondViewController.swift
@@ -8,22 +8,63 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var circle: UIView!
+    @IBOutlet weak var runButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupMyCircle()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupMyCircle() {
+        
+        circle.center.x = view.bounds.minX + circle.bounds.width / 2
+        circle.center.y = view.bounds.minY + circle.bounds.width / 2
+        circle.layer.cornerRadius = circle.frame.height / 2
+        circle.alpha = 0
+        
     }
-    */
 
+    @IBAction func runButtonPressed(_ sender: Any) {
+        let start = self.circle.center
+        
+//        UIView.animate(withDuration: 1.0, delay: 0.5, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: [.curveEaseIn]) {
+//            self.circle.transform = .identity
+//            self.circle.alpha = 1
+//
+//        } completion: { complited in
+//            UIView.animate(withDuration: 1.0, delay: 0.5, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: [.curveEaseOut], animations: {
+//                   self.circle.transform = CGAffineTransform.identity.scaledBy(x: 0.7, y: 0.7)
+//                   self.circle.alpha = 0
+//               })
+//        }
+
+        UIView.animateKeyframes(withDuration: 4, delay: 0, animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25) {
+                self.circle.transform = .identity
+                self.circle.alpha = 1
+                self.circle.center = CGPoint(x: self.view.bounds.maxX - self.circle.bounds.width / 2, y: self.view.bounds.minY + self.circle.bounds.width / 2)
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
+                self.circle.center = CGPoint(x: self.view.bounds.maxX - self.circle.bounds.width / 2, y: self.view.bounds.maxY - self.circle.bounds.width / 2)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.50, relativeDuration: 0.25) {
+                self.circle.center = CGPoint(x: self.view.bounds.minX + self.circle.bounds.width / 2, y: self.view.bounds.maxY - self.circle.bounds.width / 2)
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
+                self.circle.transform = CGAffineTransform.identity.scaledBy(x: 0.7, y: 0.7)
+                self.circle.alpha = 0
+                self.circle.center = start
+            }
+        })
+
+
+    }
+    
+    
 }
